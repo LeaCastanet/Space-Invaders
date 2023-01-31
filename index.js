5a4eba6c0e111a7835156ea27f210b0a65262eb2
Léa Castanet <lea.castanet@hotmail.com>
2023-01-31 18:06:09 +0100

    move tne player

--- index.js
+++ index.js
@@ -28,26 +28,116 @@ class Player {
   draw() {
     // c.fillStyle = "red";
     // c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+  update() {
     if (this.image) {
-      c.drawImage(
-        this.image,
-        this.position.x,
-        this.position.y,
-        this.width,
-        this.height
-      );
+      this.draw();
+      this.position.x += this.velocity.x;
     }
   }
 }
 
 const player = new Player();
-player.draw();
+const keys = {
+  q: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  ArrowLeft: {
+    pressed: false,
+  },
+  ArrowRight: {
+    pressed: false,
+  },
+  space: {
+    pressed: false,
+  },
+};
 
 function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
-  player.draw();
+  player.update();
+
+  if (keys.q.pressed || keys.ArrowLeft.pressed || keys.a.pressed) {
+    player.velocity.x = -5;
+  } else if (keys.d.pressed || keys.ArrowRight.pressed) {
+    player.velocity.x = 5;
+  } else {
+    player.velocity.x = 0;
+  }
 }
 
 animate();
+
+window.addEventListener("keydown", ({ key }) => {
+  // console.log(key);
+  switch (key) {
+    case "ArrowLeft":
+      console.log("left");
+      keys.ArrowLeft.pressed = true;
+      break;
+    case "q":
+      console.log("left");
+      keys.q.pressed = true;
+      break;
+    case "a":
+      console.log("left");
+      keys.a.pressed = true;
+      break;
+    case "ArrowRight":
+      console.log("right");
+      keys.ArrowRight.pressed = true;
+      break;
+    case "d":
+      console.log("right");
+      keys.d.pressed = true;
+      break;
+    case " ":
+      console.log("space");
+      break;
+  }
+});
+
+window.addEventListener("keyup", ({ key }) => {
+  // console.log(key);
+  switch (key) {
+    case "ArrowLeft":
+      console.log("left");
+      keys.ArrowLeft.pressed = false;
+      break;
+    case "q":
+      console.log("left");
+      keys.q.pressed = false;
+      break;
+    case "a":
+      console.log("left");
+      keys.a.pressed = false;
+      break;
+    case "ArrowRight":
+      console.log("right");
+      keys.ArrowRight.pressed = false;
+      break;
+    case "d":
+      console.log("right");
+      keys.d.pressed = false;
+      break;
+    case " ":
+      console.log("space");
+      break;
+  }
+});
