6003488c8bd1c1da4264646e0f2c2d8afd5f4ab7
Léa Castanet <lea.castanet@hotmail.com>
2023-02-03 17:02:48 +0100

    invaders shoot back

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ class Projectile {
   constructor({ position, velocity }) {
     this.position = position;
     this.velocity = velocity;
+
     this.radius = 4;
   }
 
@@ -69,6 +70,27 @@ class Projectile {
     c.fillStyle = "red";
     c.fill();
     c.closePath();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
+class InvaderProjectile {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+
+    this.width = 3;
+    this.height = 10;
+  }
+
+  draw() {
+    c.fillStyle = "white";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
   update() {
@@ -115,6 +137,21 @@ class Invader {
       this.position.x += velocity.x;
       this.position.y += velocity.y;
     }
+  }
+
+  shoot(invaderProjectiles) {
+    invaderProjectiles.push(
+      new InvaderProjectile({
+        position: {
+          x: this.position.x + this.width / 2,
+          y: this.position.y + this.height,
+        },
+        velocity: {
+          x: 0,
+          y: 5,
+        },
+      })
+    );
   }
 }
 
@@ -167,6 +204,8 @@ class Grid {
 const player = new Player();
 const projectiles = [];
 const grids = [];
+const invaderProjectiles = [];
+
 const keys = {
   q: {
     pressed: false,
@@ -196,7 +235,32 @@ function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
+
   player.update();
+
+  invaderProjectiles.forEach((invaderProjectile, index) => {
+    if (
+      invaderProjectile.position.y + invaderProjectile.height >=
+      canvas.height
+    ) {
+      setTimeout(() => {
+        invaderProjectiles.splice(index, 1);
+      }, 0);
+    } else {
+      invaderProjectile.update();
+
+      if (
+        invaderProjectile.position.y + invaderProjectile.height >=
+          player.position.y &&
+        invaderProjectile.position.x + invaderProjectile.width >=
+          player.position.x &&
+        invaderProjectile.position.x <= player.position.x + player.width
+      ) {
+        console.log("you loose");
+      }
+    }
+  });
+
   projectiles.forEach((projectile, index) => {
     if (projectile.position.y + projectile.radius <= 0) {
       setTimeout(() => {
@@ -207,8 +271,16 @@ function animate() {
     }
   });
 
-  grids.forEach((grid) => {
+  grids.forEach((grid, gridIndex) => {
     grid.update();
+
+    //spawn invaderProjectiles
+    if (frames % 100 === 0 && grid.invaders.length > 0) {
+      grid.invaders[Math.floor(Math.random() * grid.invaders.length)].shoot(
+        invaderProjectiles
+      );
+    }
+
     grid.invaders.forEach((invader, i) => {
       invader.update({ velocity: grid.velocity });
 
@@ -243,6 +315,8 @@ function animate() {
                   firstInvader.position.x +
                   lastInvader.width;
                 grid.position.x = firstInvader.position.x;
+              } else {
+                grids.splice(gridIndex, 1);
               }
             }
           }, 0);
@@ -268,7 +342,7 @@ function animate() {
     player.rotation = 0;
   }
 
-  // console.log(frames);
+  // spawning enemies
   if (frames % randomInterval === 0) {
     grids.push(new Grid());
     randomInterval = Math.floor(Math.random() * 500 + 500);
